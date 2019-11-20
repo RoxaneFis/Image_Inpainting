b896ef891de535b88abbd347b6ca07deb6cd0b24
Fischer <roxane.fischer@polytechnique.edu>
2019-11-20 10:21:49 +0100

    Ajout patchmatch

--- patchmatch.py
+++ patchmatch.py
@@ -1,20 +1,21 @@
 import cv2
 import argparse
+import numpy as np
 
 
 def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='Multiple object tracker')
     parser.add_argument('--image_input', type=str, help='file to full image to fill')
-    parser.add_argument('--image_hole', type=str, help='file to hole in full image to fill')
+    #parser.add_argument('--image_hole', type=str, help='file to hole in full image to fill')
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
+    
     args = parse_args()
-    image = cv2.imread(args.input_image)
-    hole = cv2.imread(args.image_hole)
-    image.imshow()
+    image =cv2.imread(args.image_input)
+    cv2.imshow("image",image)
+    cv2.waitKey()
 
 
-    #Initialisation : chaque pixel
