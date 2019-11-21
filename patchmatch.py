bcebf17710e2d349fcd0fb06938d608736f96cd0
Anton Raël <anton.rael@polytechnique.edu>
2019-11-21 01:52:56 +0100

    add random search

--- patchmatch.py
+++ patchmatch.py
@@ -37,7 +37,7 @@ def propagation(B,A,FNN,etape,taille):
                     if(ind_min==2):
                         A[y,x]=B[y,x-1]
 
-    if (etape%2==1): #pair
+    if (etape%2==1): #impair
         for y in range(heightA-taille-2,taille):
             for x in range(weightA-taille-2,taille):
                     PA = Patch(taille,A,y,x)
@@ -55,4 +55,25 @@ def propagation(B,A,FNN,etape,taille):
                     if(ind_min==2):
                         print("chgt")
                         A[y,x]=B[y,x+1]
+    return FNN,A
+
+def random_search(B,A,FNN,taille,scale):
+    heightA,weightA = A.shape[:2]
+    heightB,weightB = B.shape[:2]
+    for x in range(taille+scale+1,weightA-scale-taille-2):
+        for y in range(taille+scale+1,heightA-scale-taille-2):
+            min_dist=float("inf")
+            PA = Patch(taille,A,y,x)
+            ind_i=-scale
+            ind_j=-scale
+            for i in range(-scale,scale+1):
+                for j in range(-scale,scale+1):
+                    PB = Patch(taille,B,y+j,x+i)
+                    d = PA.distance(PB)
+                    if(d<min_dist):
+                        min_dist=d
+                        ind_i=i
+                        ind_j=j
+            FNN[y,x]=FNN[y+ind_j,x+ind_i]
+            A[y,x]=B[y+ind_j,x+ind_i]
     return FNN,A
