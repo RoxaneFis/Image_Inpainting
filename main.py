54d55a4ba499897856c2ca18f33cc32f5b71469b
Anton Raël <anton.rael@polytechnique.edu>
2019-11-21 01:53:05 +0100

    add etapes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import argparse
 import numpy as np
 import jupyterlab
-from patchmatch import initialisation, propagation
+from patchmatch import initialisation, propagation, random_search
 
 
 def parse_args():
@@ -15,7 +15,9 @@ def parse_args():
 
 if __name__ == '__main__':
     print("Debut")
-    taille=20
+    taille=2
+    scale=5
+    nombre_etape=3
     args = parse_args()
     B =cv2.imread(args.image_input)
     He=B
@@ -23,10 +25,14 @@ if __name__ == '__main__':
     A = B
     FNN, A = initialisation(He,B,A,taille)
     cv2.imshow("image_A_ini",A)
-    FNN,A = propagation(B,A,FNN,0,taille)
-    cv2.imshow('image_A_propag',A)
-    FNN,A = propagation(B,A,FNN,1,taille)
-    cv2.imshow('image_A_propag_2',A)
+    for etape in range(nombre_etape):
+        print("Debut etape"+str(etape))
+        print("Phase de propagation")
+        FNN,A = propagation(B,A,FNN,etape,taille)
+        cv2.imshow('image_A_propag '+str(etape),A)
+        print("Phase de random search")
+        FNN,A = random_search(B,A,FNN,taille,scale)
+        cv2.imshow('image_A_random_search '+str(etape),A)
     cv2.waitKey()
 
 
