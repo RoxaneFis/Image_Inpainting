3e4ec609ae49b22c1b7e81e2f718485fe7f7f1e1
Fischer <roxane.fischer@polytechnique.edu>
2019-12-05 13:32:11 +0100

    Patchmatch works

--- main.py
+++ main.py
@@ -15,37 +15,49 @@ def parse_args():
 
 if __name__ == '__main__':
     print("Debut")
-    taille=2
-    scale=5
-    nombre_etape=3
+    
+    #HYPERPARAMETERS
+    taille=7
+    scale=15
+    nombre_etape=10
+
+    #INITIAL IMAGE
     args = parse_args()
     B =cv2.imread(args.image_input)
     heightB,weightB = B.shape[:2]
     cv2.imshow("image_B",B)
 
+    #INITIALIZE HOLE
     x_min=int(weightB/2)
     x_max=x_min + 30
     y_min=int(heightB/2)
     y_max=y_min + 30
-
     holes_coord = {"x_min":x_min,"x_max":x_max,"y_min":y_min,"y_max":y_max}
     He = np.zeros((heightB,weightB),dtype='i')
     He[x_min:x_max,y_min:y_max]=1
 
+    #INITIALISATION
     FNN, A_padding = initialisation(He,B,taille,holes_coord)
     cv2.imshow("image_A_padding_ini",A_padding)
     C = B
     C[y_min-taille:y_max+taille+1,x_min-taille:x_max+taille+1]=A_padding
-    cv2.imshow("image_C_ini",C)
+    cv2.imshow("image_C_in",C)
 
-    # for etape in range(nombre_etape):
-    #     print("Debut etape"+str(etape))
-    #     print("Phase de propagation")
-    #     FNN,A_padding = propagation(B,A_padding,FNN,etape,taille)
-    #     cv2.imshow('image_A_propag '+str(etape),A_padding)
-    #     print("Phase de random search")
-    #     FNN,A_padding = random_search(B,A_padding,FNN,taille,scale)
-    #     cv2.imshow('image_A_random_search '+str(etape),A_padding)
+
+    for etape in range(nombre_etape):
+        FNN,A_padding = propagation(B,A_padding,He,FNN,etape,taille)
+        C[y_min-taille:y_max+taille+1,x_min-taille:x_max+taille+1]=A_padding
+        if (etape%3==0): 
+            print(f"Phase de propagation: {etape}")
+            cv2.imshow(f"A_padding_propagation_{etape}",A_padding)
+            cv2.imshow(f"C_propagation_{etape}",C)
+        
+        FNN,A_padding = random_search(B,A_padding, He,FNN,taille,scale)
+        C[y_min-taille:y_max+taille+1,x_min-taille:x_max+taille+1]=A_padding
+        if (etape%3==0): 
+            print(f"Random search {etape}")
+            cv2.imshow(f"A_padding_random_{etape}",A_padding)
+            cv2.imshow(f"C_random_{etape}",C)
     cv2.waitKey()
 
 
--- patchmatch.py
+++ patchmatch.py
@@ -3,12 +3,12 @@ import numpy as np
 import cv2
 from Patch import Patch
 
-def check_boundary(x:int, y:int,height:int,width:int)->bool:
-    return(x>=0 and x<height and y>=0 and y<width)
+def check_boundary(taille:int,y:int, x:int,height:int,width:int)->bool:
+    return(x>=taille and x<height-taille and y>=taille and y<width-taille)
 
-def not_hole(He, x:int, y:int)->bool:
+def not_hole(taille:int,He, y:int, x:int)->bool:
     height,width = He.shape[:2]
-    if(He[y,x]==1 or not check_boundary(x,y,height,width)):
+    if(not check_boundary(taille,x,y,height,width)or He[y,x]==1 ):
         return False
     else:
         return True
@@ -22,76 +22,81 @@ def initialisation(He, B, taille: int, holes_coord):
     x_max = holes_coord["x_max"]
     y_min = holes_coord["y_min"]
     y_max = holes_coord["y_max"]
-    heightA = x_max- x_min+ 1
-    widthA = y_max-y_min+1
+    heightA = y_max- y_min+ 1
+    widthA = x_max-x_min+1
     heightB,widthB = B.shape[:2]
-    FNN = np.zeros((heightA,widthA,2),dtype='i')
+    FNN = np.zeros((heightA+2*taille,widthA+2*taille,2),dtype='i')
+    for x in range(widthA+2*taille):
+        for y in range(heightA+2*taille):
+            FNN[y][x][0]=y+y_min-taille
+            FNN[y][x][1]=x+x_min-taille
+
     #Initialise A_padding = holes + boundaries
     A_padding = np.zeros((heightA+2*taille,widthA+2*taille))
     A_padding = B[y_min-taille:y_max+taille+1,x_min-taille:x_max+taille+1]
+    
     for x in range(taille, widthA+taille):
         for y in range(taille, heightA+taille):
             xB = random.randint(taille,widthB-1-taille)
             yB = random.randint(taille,heightB-1-taille)
-            while (not not_hole(He,xB,yB)):
+            while (not not_hole(taille,He,yB,xB)):
                 xB = random.randint(taille,widthB-1-taille)
                 yB = random.randint(taille,heightB-1-taille)
-            #Initialise Fnn with random patch values
-            FNN[y-taille][x-taille][0]=yB
-            FNN[y-taille][x-taille][1]=xB
+            #Initialise Fnn with random patch values (not in hole)
+            FNN[y][x][0]=yB
+            FNN[y][x][1]=xB
             #Initialise A with random values
             A_padding[y,x]=B[yB,xB]
     return FNN,A_padding
 
-def propagation(B,A,FNN,etape,taille):
-    heightA,widthA = A.shape[:2]
+def propagation(B,A_padding,He,FNN,etape,taille):
+    heightA_padding,widthA_padding = A_padding.shape[:2]
+    heightA,widthA = heightA_padding-2*taille,widthA_padding-2*taille
     heightB,widthB = B.shape[:2]
     if (etape%2==0): #pair
-        for x in range(taille+1,widthA-taille-1):
-            for y in range(taille+1,heightA-taille-1):
-                    PA = Patch(taille,A,y,x)
+        for x in range(taille, widthA+taille):
+            for y in range(taille, heightA+taille):
+                    PA = Patch(taille,A_padding,y,x)
                     voisins=[[y,x],[y-1,x],[y,x-1]]
                     #TO DO : essayer avec les voisins de FNN[y-1,x] et FNN[y,x-1] (attention aux bords)
-                    voisins_B = [FNN[y,x],FNN[y-1,x]+[1,0],FNN[y,x-1]+[0,1]]
-                    if(voisins_B[1][0]>heightB-taille-1):
-                        voisins_B[1]=FNN[y-1,x]
-                    if(voisins_B[2][1]>widthB-taille-1):
-                        voisins_B[2]=FNN[y,x-1]
+                    voisins_B = [FNN[y,x],FNN[y-1,x],FNN[y,x-1]]
+                    voisins_B = [indices for indices in voisins_B if not_hole(taille,He, indices[0], indices[1]) ]
                     distances=[]
-                    for i in range(3):
-                        PB = Patch(taille,B,voisins_B[i][0],voisins_B[i][1])
+                    for voisin in voisins_B:
+                        PB = Patch(taille,B,voisin[0],voisin[1])
                         distances.append(PA.distance(PB))
                     ind_min = np.argmin(distances)
                     FNN[y,x]=voisins_B[ind_min]
                     yB = FNN[y][x][0]
                     xB = FNN[y][x][1]
-                    A[y,x]=B[yB,xB]
+                    A_padding[y,x]=B[yB,xB]
 
     if (etape%2==1): #impair
-        for y in range(heightA-taille-2,taille):
-            for x in range(widthA-taille-2,taille):
-                    PA = Patch(taille,A,y,x)
+        for y in range(heightA+taille,taille):
+            for x in range(widthA+taille,taille):
+                    PA = Patch(taille,A_padding,y,x)
                     voisins=[[y,x],[y+1,x],[y,x+1]]
                     voisins_B = [FNN[y,x],FNN[y+1,x],FNN[y,x+1]]
+                    voisins_B = [indices for indices in voisins_B if not_hole(taille,He, indices[0], indices[1]) ]                    
                     distances=[]
-                    for i in range(3):
-                        PB = Patch(taille,B,voisins_B[i][0],voisins_B[i][1])
+                    for voisin in voisins_B:
+                        PB = Patch(taille,B,voisin[0],voisin[1])
                         distances.append(PA.distance(PB))
                     ind_min = np.argmin(distances)
                     FNN[y,x]=voisins_B[ind_min]
                     yB = FNN[y][x][0]
                     xB = FNN[y][x][1]
-                    A[y,x]=B[yB,xB]
-    return FNN,A
+                    A_padding[y,x]=B[yB,xB]
+    return FNN,A_padding
 
-def random_search(B,A,FNN,taille,scale):
+def random_search(B,A_padding,He,FNN,taille,scale):
     #Phase à modifier : modification taille de la fenetre à diminuer au sein d'une même étape + RANDOM
-    heightA,widthA = A.shape[:2]
+    heightA_padding,widthA_padding = A_padding.shape[:2]
+    heightA,widthA = heightA_padding-2*taille,widthA_padding-2*taille
     heightB,widthB = B.shape[:2]
-
-    for x in range(taille+scale+1,widthA-scale-taille-2):
-        for y in range(taille+scale+1,heightA-scale-taille-2):
-            PA = Patch(taille,A,y,x)
+    for x in range(taille, widthA+taille):
+        for y in range(taille, heightA+taille):
+            PA = Patch(taille,A_padding,y,x)
             scale_reduce = scale
             while (scale_reduce >=1):
                 xB = FNN[y,x][1]
@@ -99,13 +104,21 @@ def random_search(B,A,FNN,taille,scale):
                 rx = random.randint(-scale,scale)
                 ry = random.randint(-scale,scale)
                 #TO Do : vérifier que rx et ry ne sont pas dans le trou ou en dehors du bord
-                
+                while (not not_hole(taille,He,yB+ry,xB+rx)):
+                    rx = random.randint(-scale,scale)
+                    ry = random.randint(-scale,scale)
                 PB = Patch(taille,B,yB,xB)
                 P_Potentiel = Patch(taille,B,yB+ry,xB+rx)
+                # print("PB_______")
+                # print(PB.mat)
+                # print("P_Potentiel_______")
+                # print(P_Potentiel.mat)
+                # print(f"yb {yB}, xB {xB} ,yB+ry {yB+ry},xB+rx {xB+rx}")
+                # print()
                 if PA.distance(P_Potentiel)<PA.distance(PB):
                     FNN[y][x][0]=yB+ry
                     FNN[y][x][1]=xB+rx
-                    A[y,x]=B[yB+ry,xB+rx]
+                    A_padding[y,x]=B[yB+ry,xB+rx]
 
                 scale_reduce=scale_reduce//2
-    return FNN,A
+    return FNN,A_padding
