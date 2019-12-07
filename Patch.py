12d8f5edd650896bd53ca3eeac9323f888220024
Anton Raël <anton.rael@polytechnique.edu>
2019-12-07 13:08:15 +0100

    add gradient in distance

--- Patch.py
+++ Patch.py
@@ -6,10 +6,10 @@ class Patch:
     def __init__(self,taille,matrice,y,x):
         self.taille = taille
         self.mat = matrice[y-taille:y+taille+1,x-taille:x+taille+1]
-        #self.grad = gradient(self.mat)
+        self.grad = gradient(self.mat)
     def distance(self, P2):
         d_color = cv2.norm(self.mat,P2.mat)
-        #d_gradient =  cv2.norm(self.grad,P2.grad)
-        #d = alpha[0]*d_color+alpha[1]*d_gradient
+        d_gradient =  cv2.norm(self.grad,P2.grad)
+        d = alpha[0]*d_color+alpha[1]*d_gradient
         return d_color
         
